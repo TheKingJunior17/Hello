bf5668846936ce41c1f92f745efc421f5b40400e
TheKingJunior17 <119756262+TheKingJunior17@users.noreply.github.com>
2025-03-23 23:09:40 -0700

    Update data_structures.h

--- data_structures.h
+++ data_structures.h
@@ -1,1 +1,15 @@
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURES_H
 
+struct ELEMENT {
+    double key;
+    // Add any other fields if necessary
+};
+
+struct HEAP {
+    int size;
+    int capacity;
+    ELEMENT **array;
+};
+
+#endif
