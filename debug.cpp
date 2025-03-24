de8386d50561c3b866ad398d692470336be8f02d
TheKingJunior17 <119756262+TheKingJunior17@users.noreply.github.com>
2025-03-23 23:29:07 -0700

    Add files via upload

---
+++ debug.cpp
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * FILE NAME: degub.cpp
+ * DATE: 2013-10-16
+ * AUTHOR: Zachary Priddy
+ *              me@zpriddy.com
+ *              zpriddy@asu.edu
+ * 
+ * DESC:
+ * 
+ * 
+ ******************************************************************************/
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+//Change this to 1 to display debug messages
+int DEBUG=0;
+
+
+
+void debug(string out)
+{
+    if(DEBUG == 1)
+    {
+        cout << "DEBUG:\n";
+        cout << out << "\n";
+    }
+}
+
+void debug(int out)
+{
+    if(DEBUG == 1)
+    {
+        cout << "DEBUG:\n";
+        cout << out << "\n";
+    }
+}
+
+void debug(string desc, int out)
+{
+    if(DEBUG == 1)
+    {
+        cout << "DEBUG: " << desc << "\n";
+        cout << out << "\n";
+    }
+}
